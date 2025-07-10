f1373d011943c776d6fc032a01ef59a567e78d83
inamyadav <inamyadav23@gmail.com>
2025-07-10 08:35:57 +0530

    authentication done

--- server/routes/FAQ.routers.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getAllFAQs } from "../controllers/FAQ.controllers.js";
-const router = express.Router();
-
-router.get("/faqs", getAllFAQs); // Route to get all FAQs
-
-export default router;
--- server/routes/searchRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { searchHackathons } from "../controllers/searchController.js";
-
-const router = express.Router();
-
-//  Search Hackathons
-router.get("/search", searchHackathons);
-
-export default router;
